e7d558ffa6a43100465f817c8d31a2c2cab7e138
arielbider <arik.bider@gmail.com>
2020-09-26 23:57:59 +0300

    Version 1.0 to Stable

--- cvfr-map/route.js
+++ cvfr-map/route.js
@@ -9,25 +9,24 @@ function addWaypoint(id, if_airport) {
     translteRoute();
 }
 
-function removeWaypoint(id) {
-	
+function removeWaypoint(id, if_airport) {
+
     var route_string = document.getElementById("route").innerHTML;
-	
+
     if (route_string != "") {
 
         ICAO_code = id.replaceAll(" ", "");
 
         var index = route_string.lastIndexOf(ICAO_code),
-        route_string_as_Array = Array.from(route_string),
-        last_char = ICAO_code.length;
-		
+            route_string_as_Array = Array.from(route_string),
+            last_char = ICAO_code.length;
+
         for (var i = 0; i < last_char; i++) {
             route_string_as_Array.splice(index, 1);
         }
-		
-		console.log(route_string_as_Array);
-		document.getElementById("route").innerHTML = route_string_as_Array.join('');
-        updatePopup(id);
+
+        document.getElementById("route").innerHTML = route_string_as_Array.join('');
+        updatePopup(id, if_airport);
         translteRoute();
     }
 }
@@ -48,14 +47,14 @@ function translteRoute() {
 
         var latlongs = Array();
 
-        ICAO_codes.forEach(function (code) {
+        ICAO_codes.forEach(function(code) {
             code = code.replaceAll(" ", "");
             code = code.replaceAll("&nbsp;", "");
             var waypoint_data = Object.values(cvfr_waypoints).filter(waypoint => waypoint.CODE == code);
 
             if (waypoint_data.length != 0) {
                 var lat = waypoint_data[0].LAT,
-                lng = waypoint_data[0].LONG;
+                    lng = waypoint_data[0].LONG;
                 latlongs.push([lat, lng]);
             }
         });
@@ -63,26 +62,21 @@ function translteRoute() {
         route_polyline.setLatLngs(latlongs);
         addAirportsToRoutePolyline();
     } else {
-        console.log(route);
         route_polyline.setLatLngs([]);
         addAirportsToRoutePolyline();
     }
 }
 
-var dep = "", arr = "";
+var dep = "",
+    arr = "";
 
 function validateAirport(id) {
 
     var airport_code = document.getElementById(id).innerHTML;
 
-    console.log(id);
-    console.log(airport_code.length);
-    console.log(dep.length);
-
     if (id == "departure" && ((airport_code.length == 3 && dep.length == 4) || (airport_code.length == 5 && dep.length == 4) || (dep.length - airport_code.length > 1)) && route_polyline.getLatLngs().length > 0) {
         var new_polyline = route_polyline.getLatLngs();
         new_polyline.splice(0, 1);
-        console.log(new_polyline);
         route_polyline.setLatLngs(new_polyline);
         dep = airport_code;
 
@@ -115,8 +109,8 @@ function validateAirport(id) {
 
 function addAirportsToRoutePolyline() {
     var dep = document.getElementById("departure").innerHTML,
-    arr = document.getElementById("arrival").innerHTML,
-    array_of_points = route_polyline.getLatLngs();
+        arr = document.getElementById("arrival").innerHTML,
+        array_of_points = route_polyline.getLatLngs();
 
     if (dep != "") {
         var airport = Object.values(cvfr_waypoints).filter(waypoint => waypoint.CODE == dep);
@@ -141,41 +135,39 @@ function addAirportsToRoutePolyline() {
 function setAsDeparture(airport_code) {
     document.getElementById("departure").innerHTML = airport_code;
     validateAirport("departure");
-	updatePopup(airport_code, 1);
+    updatePopup(airport_code, 1);
 }
 
 function setAsArrival(airport_code) {
-    console.log(airport_code);
     document.getElementById("arrival").innerHTML = airport_code;
     validateAirport("arrival");
-	updatePopup(airport_code, 1);
+    updatePopup(airport_code, 1);
 }
 
 function updatePopup(airport_code, if_airport) {
-    let route = document.getElementById("route").innerHTML;
-	console.log(route);
-    let dep = document.getElementById("departure").innerHTML;
-	console.log(dep);
-    let arr = document.getElementById("arrival").innerHTML;
-	console.log(arr);
-    let marker_info = marker.getContent().split("<br>");
-	console.log(airport_code);
-	if (!if_airport){
-		marker_info[3] = marker_info[3].replace("</div>", "");
-		
-	console.log(marker_info[3]);
-	}
-    marker_info = marker_info[0] + "<br>" + marker_info[1] + "<br>" + marker_info[2] + "<br>" + marker_info[3];
-	
+    let route = document.getElementById("route").innerHTML, dep = document.getElementById("departure").innerHTML,
+    arr = document.getElementById("arrival").innerHTML, marker_info = marker.getContent().split("<div>");
+
+    marker_info = marker_info[0] + "<div>" + marker_info[1];
+
     if (route.includes(airport_code)) {
-        marker_info += `<br><button type="button" id="${airport_code}" onclick="removeWaypoint(this.id)" class="marker-button">הסר מנתיב</button>`;
+        marker_info += `<div>
+          <img src="img/plan-normal.png" data-waypoint="${airport_code}" class="popup-button" onclick="removeWaypoint(this.dataset.waypoint, ${if_airport})" onmouseover="this.src='img/plan-hover.png'" onmouseout="this.src='img/plan-normal.png'">
+          <span class="button-text">הסר מנתיב</span>
+        </div>`;
     }
     if (dep != airport_code && if_airport) {
         //הוספת כפתור קבע כשדה המראה
-        marker_info += `<br><button type="button" onclick="setAsDeparture(this.dataset.airport)" data-airport="${airport_code}" class="marker-button">קבע כשדה המראה</button>`;
+        marker_info += `<div>
+          <img src="img/plan-normal.png" onclick="setAsDeparture(this.dataset.airport)" data-airport="${airport_code}" class="popup-button" onmouseover="this.src='img/plan-hover.png'" onmouseout="this.src='img/plan-normal.png'">
+          <span class="button-text">קבע כשדה המראה</span>
+          </div>`;
     }
     if (arr != airport_code && if_airport) {
-        marker_info += `<br><button type="button" onclick="setAsArrival(this.dataset.airport)" data-airport="${airport_code}" class="marker-button">קבע כשדה נחיתה</button>`;
+        marker_info += `<div>
+          <img src="img/plan-normal.png" onclick="setAsArrival(this.dataset.airport)" data-airport="${airport_code}" class="popup-button" onmouseover="this.src='img/plan-hover.png'" onmouseout="this.src='img/plan-normal.png'">
+          <span class="button-text">קבע כשדה נחיתה</span>
+        </div>`;
     }
 
     marker_info + "</div>";
@@ -183,7 +175,10 @@ function updatePopup(airport_code, if_airport) {
 }
 
 var div = document.getElementById('planner');
-var box_height = div.offsetHeight, box_width = div.offsetWidth, window_height = window.innerHeight, window_width = window.innerWidth;
+var box_height = div.offsetHeight,
+    box_width = div.offsetWidth,
+    window_height = window.innerHeight,
+    window_width = window.innerWidth;
 
 window.onload = addListeners();
 var offX;

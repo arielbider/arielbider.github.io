9cda2f919638ba6f07869a736ea1dffad4669e51
arielbider <arik.bider@gmail.com>
2020-09-30 19:13:56 +0300

    V1.2 to Dev
    Added CVFR map

--- cvfr-map-dev/map.js
+++ cvfr-map-dev/map.js
@@ -119,7 +119,7 @@ function deg2rad(deg) {
 
 var map = L.map('mapid', {
     minZoom: 8,
-    maxZoom: 17
+    maxZoom: 14
 });
 
 var marker;
@@ -138,78 +138,24 @@ map.on('popupopen', function(source) {
     }
 });
 
-var osm_map_layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'osm',
+var cvfr_map = L.tileLayer('map/{z}/{x}/{y}.png', {
+    attribution: 'מקור: &copy; פמ"ת פנים ארצי, רת"א | עיבוד: בר רודוי ואריאל בידר',
 }).addTo(map);
 
-var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    maxZoom: 17,
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-});
+var waypoints_layer = new L.FeatureGroup().addTo(map);
 
-var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-});
+for (let waypoint_key in cvfr_waypoints) {
+    let waypoint = cvfr_waypoints[waypoint_key];
 
-var waypoints_must_layer = new L.FeatureGroup().addTo(map),
-    waypoints_by_request_layer = new L.FeatureGroup().addTo(map),
-    waypoints_switch_atc_layer = new L.FeatureGroup().addTo(map),
-    civil_airways_layer = new L.FeatureGroup().addTo(map),
-    military_airways_layer = new L.FeatureGroup().addTo(map),
-    altitude_pins_layer = new L.FeatureGroup().addTo(map);
-
-for (var waypoint_key in cvfr_waypoints) {
-    var waypoint = cvfr_waypoints[waypoint_key];
-    let curr_marker;
-
-    if (waypoint["atc_switch"]) {
+    if (waypoint["type"] == "ARP") {
         curr_marker = L.marker([waypoint["LAT"], waypoint["LONG"]], {
-                icon: new L.DivIcon({
-                    iconSize: [120, 20],
-                    iconAnchor: [60, 10],
-                    className: 'waypoint-marker',
-                    html: `<div class="waypoint-div"><img class="waypoint-icon" src="map_pins/must_switch.png"/><br><span class="waypoint-name">${waypoint["שם"]}</span></div></div>`
-                })
+            icon: new L.DivIcon({
+                iconSize: [256, 256],
+                iconAnchor: [128, 128],
+                className: 'waypoint-marker',
             })
-            .addTo(waypoints_switch_atc_layer);
+        }).addTo(waypoints_layer);
         curr_marker.bindPopup(`<div class="waypoint_popup" id="${waypoint["CODE"]}-div"><div class="airport-name">${waypoint["שם"]} - ${waypoint["CODE"]}</div>
-          <div>
-            <img src="img/plan-normal.png" data-waypoint="${waypoint["CODE"]}" class="popup-button" onclick="addWaypoint(this.dataset.waypoint, 0)" onmouseover="this.src='img/plan-hover.png'" onmouseout="this.src='img/plan-normal.png'">
-            <span class="button-text">הוסף לנתיב</span>
-          </div>
-        </div>`);
-    } else {
-        switch (waypoint["סוג דיווח"]) {
-            case "דרישה":
-                curr_marker = L.marker([waypoint["LAT"], waypoint["LONG"]], {
-                    icon: new L.DivIcon({
-                        className: 'waypoint-marker',
-                        iconSize: [120, 20],
-                        iconAnchor: [60, 10],
-                        html: `<div class="waypoint-div"><img class="waypoint-icon" src="map_pins/by_request.png"/><br><span class="waypoint-name">${waypoint["שם"]}</span></div>`
-                    })
-                }).addTo(waypoints_by_request_layer);
-                curr_marker.bindPopup(`<div class="waypoint_popup" id="${waypoint["CODE"]}-div"><div class="airport-name">${waypoint["שם"]} - ${waypoint["CODE"]}</div>
-                    <div>
-                      <img src="img/plan-normal.png" data-waypoint="${waypoint["CODE"]}" class="popup-button" onclick="addWaypoint(this.dataset.waypoint, 0)" onmouseover="this.src='img/plan-hover.png'" onmouseout="this.src='img/plan-normal.png'">
-                      <span class="button-text">הוסף לנתיב</span>
-                    </div>
-                  </div>`);
-                break;
-            case "ARP":
-                curr_marker = L.marker([waypoint["LAT"], waypoint["LONG"]], {
-                    icon: new L.DivIcon({
-                        iconSize: [120, 20],
-                        iconAnchor: [60, 10],
-                        className: 'waypoint-marker',
-                        html: `<div class="waypoint-div"><img class="waypoint-icon" src="map_pins/airport.png"/><br><span class="waypoint-name">${waypoint["שם"]}</span></div>`
-                    })
-                }).addTo(waypoints_by_request_layer);
-                curr_marker.bindPopup(`<div class="waypoint_popup" id="${waypoint["CODE"]}-div"><div class="airport-name">${waypoint["שם"]} - ${waypoint["CODE"]}</div>
                     <div>
                       <img src="img/plan-normal.png" data-waypoint="${waypoint["CODE"]}" class="popup-button" onclick="addWaypoint(this.dataset.waypoint, 1)" onmouseover="this.src='img/plan-hover.png'" onmouseout="this.src='img/plan-normal.png'">
                       <span class="button-text">הוסף לנתיב</span>
@@ -223,253 +169,24 @@ for (var waypoint_key in cvfr_waypoints) {
                       <span class="button-text">קבע כשדה נחיתה</span>
                     </div>
                   </div>`, {
-                    minWidth: 170
-                });
-                break;
-            case "חובה":
-                curr_marker = L.marker([waypoint["LAT"], waypoint["LONG"]], {
-                    icon: new L.DivIcon({
-                        iconSize: [120, 20],
-                        iconAnchor: [60, 10],
-                        className: 'waypoint-marker',
-                        html: `<div class="waypoint-div"><img class="waypoint-icon" src="map_pins/must.png"/><br><span class="waypoint-name">${waypoint["שם"]}</span></div>`
-                    })
-                }).addTo(waypoints_must_layer);
-                curr_marker.bindPopup(`<div class="waypoint_popup" id="${waypoint["CODE"]}-div"><div class="airport-name">${waypoint["שם"]} - ${waypoint["CODE"]}</div>
+            minWidth: 170
+        });
+    } else {
+        curr_marker = L.marker([waypoint["LAT"], waypoint["LONG"]], {
+            icon: new L.DivIcon({
+                iconSize: [120, 120],
+                iconAnchor: [60, 60],
+                className: 'waypoint-marker',
+            })
+        }).addTo(waypoints_layer);
+        curr_marker.bindPopup(`<div class="waypoint_popup" id="${waypoint["CODE"]}-div"><div class="airport-name">${waypoint["שם"]} - ${waypoint["CODE"]}</div>
                     <div>
                       <img src="img/plan-normal.png" data-waypoint="${waypoint["CODE"]}" class="popup-button" onclick="addWaypoint(this.dataset.waypoint, 0)" onmouseover="this.src='img/plan-hover.png'" onmouseout="this.src='img/plan-normal.png'">
                       <span class="button-text">הוסף לנתיב</span>
                     </div>
                   </div>`);
-        }
     }
 }
-
-airways.forEach(function(airway) {
-
-    var start_point_LAT = cvfr_waypoints[airway.start_point].LAT,
-        start_point_LONG = cvfr_waypoints[airway.start_point].LONG;
-    var end_point_LAT = cvfr_waypoints[airway.end_point].LAT,
-        end_point_LONG = cvfr_waypoints[airway.end_point].LONG;
-
-    var bearing = bearingCalculator.bearing(cvfr_waypoints[airway.start_point].LAT, cvfr_waypoints[airway.start_point].LONG, cvfr_waypoints[airway.end_point].LAT, cvfr_waypoints[airway.end_point].LONG);
-    bearing = ~~bearing
-
-    var start_point, end_point;
-
-    var distance = getDistance(cvfr_waypoints[airway.start_point].LAT, cvfr_waypoints[airway.start_point].LONG, cvfr_waypoints[airway.end_point].LAT, cvfr_waypoints[airway.end_point].LONG);
-
-    if (distance > 25) {
-        start_point = destinationPoint({
-            lat: start_point_LAT,
-            lng: start_point_LONG
-        }, bearing, distance * 0.02);
-        end_point = destinationPoint({
-            lat: end_point_LAT,
-            lng: end_point_LONG
-        }, (bearing - 180), distance * 0.02);
-    } else if (distance > 20) {
-        start_point = destinationPoint({
-            lat: start_point_LAT,
-            lng: start_point_LONG
-        }, bearing, distance * 0.05);
-        end_point = destinationPoint({
-            lat: end_point_LAT,
-            lng: end_point_LONG
-        }, (bearing - 180), distance * 0.05);
-    } else {
-        start_point = destinationPoint({
-            lat: start_point_LAT,
-            lng: start_point_LONG
-        }, bearing, distance * 0.1);
-        end_point = destinationPoint({
-            lat: end_point_LAT,
-            lng: end_point_LONG
-        }, (bearing - 180), distance * 0.1);
-    }
-    var latlngs = Array();
-    latlngs.push(start_point);
-    latlngs.push(end_point);
-
-    var airway_color = function() {
-        if (airway["army_airway"]) {
-            return 'rgb(0,114,188)'; //blue
-        }
-        return 'rgb(0,166,81)'; // green
-    };
-
-    let polyline;
-
-    if (airway["on_atc_approval"]) {
-        polyline = L.polyline(latlngs, {
-            color: airway_color(),
-            weight: 6,
-            dashArray: '5, 15',
-            lineCap: 'square'
-        });
-    } else {
-        polyline = L.polyline(latlngs, {
-            color: airway_color(),
-            weight: 6
-        });
-    }
-
-    let layerToAddTo = function() {
-        if (airway["army_airway"]) {
-            return military_airways_layer;
-        } else {
-            return civil_airways_layer;
-        }
-    }
-
-    polyline.addTo(layerToAddTo());
-
-    if (airway["one_way"]) {
-        let arrowHead = L.polylineDecorator(polyline, {
-            patterns: [{
-                offset: '100%',
-                repeat: 0,
-                symbol: L.Symbol.arrowHead({
-                    pixelSize: 20,
-                    polygon: false,
-                    pathOptions: {
-                        stroke: true,
-                        color: airway_color(),
-                        weight: 6
-                    }
-                })
-            }]
-        }).addTo(layerToAddTo());
-    }
-
-    // make altitude marker
-
-    var rotation_degree = angleFromCoordinate(start_point[0], start_point[1], end_point[0], end_point[1]);
-
-    if (airway["to_altitude"] == airway["from_altitude"]) {
-        let html = "";
-        let altitude_html = function() {
-            if (rotation_degree < -90 || rotation_degree > 90) {
-                html += `<div class="altitude-div" style ="transform: rotate(${rotation_degree}deg);"><img src="map_pins/equal_altitude.png" style="width: 100%"><div style="transform: rotate(180deg);"><div class="altitude-text" style="transform: translate(-50%, 10%);">${airway["to_altitude"]}</div></div>`;
-            } else {
-                html += `<div class="altitude-div" style ="transform: rotate(${rotation_degree}deg);"><img src="map_pins/equal_altitude.png" style="width: 100%"><div class="altitude-text" style="transform: translate(-50%, -60%)">${airway["to_altitude"]}</div></div>`;
-            }
-            return html;
-        };
-
-        //make the altitude marker
-        var midPoint = middlePoint(start_point[0], start_point[1], end_point[0], end_point[1]);
-
-        L.marker(midPoint, {
-                icon: new L.DivIcon({
-                    className: 'altitude-marker',
-                    html: altitude_html(),
-                    iconSize: [76, 76],
-                    iconAnchor: [38, 38]
-                })
-            })
-            .addTo(altitude_pins_layer);
-
-        let bearingToText = function(bearing) {
-            let html = "";
-            if (rotation_degree < -90 || rotation_degree > 90) {
-                html += `<div class="altitude-div" style ="transform: rotate(${rotation_degree}deg);"><div class="bearing" style="transform: rotate(180deg)">${bearing}</div></div>`;
-            } else {
-                html += `<div class="altitude-div" style ="transform: rotate(${rotation_degree}deg);"><div class="bearing">${bearing}</div></div>`;
-            }
-            return html;
-        };
-        bearingText = bearingFormat(bearing);
-        //make bearing 1
-        L.marker(start_point, {
-                icon: new L.DivIcon({
-                    className: 'altitude-marker',
-                    html: bearingToText(bearingText),
-                    iconSize: [30, 30],
-                    iconAnchor: [0, 0]
-                })
-            })
-            .addTo(altitude_pins_layer);
-
-        //make bearing 2
-
-        var bearing_opposite = bearingCalculator.bearing(cvfr_waypoints[airway.end_point].LAT, cvfr_waypoints[airway.end_point].LONG, cvfr_waypoints[airway.start_point].LAT, cvfr_waypoints[airway.start_point].LONG);
-        bearing_opposite = ~~bearing_opposite
-        bearingText_opposite = bearingFormat(bearing_opposite);
-
-        L.marker(end_point, {
-                icon: new L.DivIcon({
-                    className: 'altitude-marker',
-                    html: bearingToText(bearingText_opposite),
-                    iconSize: [30, 30],
-                    iconAnchor: [0, 0]
-                })
-            })
-            .addTo(altitude_pins_layer);
-
-    } else {
-        bearingText = bearingFormat(bearing);
-
-        var altitude_html = function() {
-            var rotation_degree = angleFromCoordinate(start_point[0], start_point[1], end_point[0], end_point[1]);
-            let html = "";
-            if (rotation_degree < -90 || rotation_degree > 90) {
-                html += `<div class="altitude-div" style ="transform: rotate(${rotation_degree}deg);"><div class="bearing" style="transform: rotate(180deg)">${bearingText}</div><img src="map_pins/altitude_level.png" style="width: 100%"><div style="transform: rotate(180deg);"><div class="altitude-text" style="transform: translate(-50%, 10%);">${airway["to_altitude"]}</div></div>`;
-            } else {
-                html += `<div class="altitude-div" style ="transform: rotate(${rotation_degree}deg);"><img src="map_pins/altitude_level.png" style="width: 100%"><div class="altitude-text">${airway["to_altitude"]}</div><div class="bearing">${bearingText}</div></div>`;
-
-            }
-            return html;
-        };
-
-        let start_point = destinationPoint({
-            lat: start_point_LAT,
-            lng: start_point_LONG
-        }, bearing, distance * 0.2);
-        let end_point = destinationPoint({
-            lat: end_point_LAT,
-            lng: end_point_LONG
-        }, (bearing - 180), distance * 0.2)
-
-        L.marker(start_point, {
-            icon: new L.DivIcon({
-                className: 'altitude-marker',
-                html: altitude_html(),
-                iconSize: [76, 76],
-                iconAnchor: [38, 38]
-            })
-        }).addTo(altitude_pins_layer);
-
-        //make opposite direction altitude marker if such exists
-
-        if (!airway["one_way"]) {
-            var bearing_opposite = bearingCalculator.bearing(cvfr_waypoints[airway.end_point].LAT, cvfr_waypoints[airway.end_point].LONG, cvfr_waypoints[airway.start_point].LAT, cvfr_waypoints[airway.start_point].LONG);
-            bearing_opposite = ~~bearing_opposite
-            bearingText_opposite = bearingFormat(bearing_opposite);
-
-            let altitude_html = function() {
-                let rotation_degree = angleFromCoordinate(end_point[0], end_point[1], start_point[0], start_point[1]);
-                let html = "";
-                if (rotation_degree < -90 || rotation_degree > 90) {
-                    html += `<div class="altitude-div" style ="transform: rotate(${rotation_degree}deg);"><div class="bearing" style="transform: rotate(180deg)">${bearingText_opposite}</div><img src="map_pins/altitude_level.png" style="width: 100%"><div style="transform: rotate(180deg);"><div class="altitude-text" style="transform: translate(-50%, 10%);">${airway["from_altitude"]}</div></div>`;
-                } else {
-                    html += `<div class="altitude-div" style ="transform: rotate(${rotation_degree}deg);"><img src="map_pins/altitude_level.png" style="width: 100%"><div class="altitude-text">${airway["from_altitude"]}</div><div class="bearing">${bearingText_opposite}</div></div>`;
-
-                }
-                return html;
-            };
-
-            L.marker(end_point, {
-                icon: new L.DivIcon({
-                    className: 'altitude-marker',
-                    html: altitude_html(),
-                    iconSize: [76, 76],
-                    iconAnchor: [38, 38]
-                })
-            }).addTo(altitude_pins_layer);
-        }
-    }
-});
 
 function resizeLayers(ClassName, multiplyBy) {
     var altitude_markers = document.getElementsByClassName(ClassName);
@@ -511,28 +228,6 @@ map.on('zoomend', function() {
     resizeLayers("altitude-div", multiplyBy);
     resizeLayers("waypoint-div", multiplyBy);
 });
-
-// Bring some layers to Front
-civil_airways_layer.bringToBack();
-military_airways_layer.bringToBack();
-altitude_pins_layer.bringToBack();
-waypoints_must_layer.bringToFront();
-waypoints_by_request_layer.bringToFront();
-waypoints_switch_atc_layer.bringToFront();
-
-// Add Layer Control
-L.control.layers({
-    "OSM": osm_map_layer,
-    "לוויני": Esri_WorldImagery,
-    "מפה טופוגרפית": OpenTopoMap
-}, {
-    "דיווח חובה": waypoints_must_layer,
-    "דיווח לפי דרישה": waypoints_by_request_layer,
-    "נקודות מעבר פיקוח": waypoints_switch_atc_layer,
-    "נתיבים אזרחיים": civil_airways_layer,
-    "נתיבים צבאיים": military_airways_layer,
-    "גבהי טיסה": altitude_pins_layer
-}).addTo(map);
 
 map.setView([32.00944444, 34.88555556], 13);
 

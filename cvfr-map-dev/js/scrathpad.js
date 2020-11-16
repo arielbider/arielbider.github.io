0ed204f895fc9143d22d7f213d5be3b6dc9aa3a8
Ariel Bider <arik.bider@gmail.com>
2020-11-16 20:03:59 +0200

    Final Fix to scrathpad logic for resizing

--- cvfr-map-dev/js/scrathpad.js
+++ cvfr-map-dev/js/scrathpad.js
@@ -6,9 +6,14 @@ var signaturePad = new SignaturePad(canvas, {
 
 function resizeCanvas() {
 	var ratio = Math.max(window.devicePixelRatio || 1, 1);
+
+	const data = signaturePad.toData();
+
 	canvas.width = canvas.offsetWidth * ratio;
 	canvas.height = canvas.offsetHeight * ratio;
 	canvas.getContext("2d").scale(ratio, ratio);
+	
+	signaturePad.fromData(data);
 	// signaturePad.clear(); //otherwise isEmpty() might return incorrect value
 }
 

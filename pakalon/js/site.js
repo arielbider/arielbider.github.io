2bf237d2b32461bd4457265a2a9330b5a3237fb7
Ariel Bider <arik.bider@gmail.com>
2021-08-27 16:26:42 +0300

    [pakalon] added window asking for name

--- pakalon/js/site.js
+++ pakalon/js/site.js
@@ -1,4 +1,23 @@
 window.addEventListener('load', (event) => {
+  // Request name from user
+  if(document.cookie){
+    name = getCookie("name");
+    document.getElementById("name").innerHTML = "שלום " + name;
+  } else {
+    const name = prompt("הכניסו את שמכם המלא", "");
+    if(name){
+      document.getElementById("name").innerHTML = "שלום " + name;
+
+      const d = new Date();
+      d.setTime(d.getTime() + (60*60*1000));
+      let expires = "expires="+ d.toUTCString();
+      document.cookie = "name=" + name + ";" + expires + ";";
+    } else {
+      document.getElementById("name").remove()
+    }
+  }
+
+  // Add to tds the function of coloring
   tds = document.getElementsByTagName('td');
 
   for(td of tds){
@@ -17,3 +36,19 @@ window.addEventListener('load', (event) => {
     });
   }
 });
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
